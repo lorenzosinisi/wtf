8fbe3493f9d8364fa54cf0ab5d0331036c15239f
Lorenzo Sinisi <lasslo.net@gmail.com>
2015-12-28 01:45:53 +0100

    added some core function utily

--- lib/wtf/core.rb
+++ lib/wtf/core.rb
@@ -1,4 +1,31 @@
 module Wtf
   class Core
+    class << self
+      
+      def current_path
+        Dir.pwd
+      end
+
+      def has_documentation?
+        File.exist?(current_path + wtf_file)
+      end
+
+      alias :is_documented? :has_documentation?
+
+      def wtf_file
+        '/.wtf'
+      end
+
+      def clean
+        if is_documented?
+          File.delete(current_path + wtf_file)
+          true
+        else
+          puts "This folder was not documented, nothing deleted."
+          nil
+        end
+      end
+
+    end
   end
 end
--- spec/wtf/core_spec.rb
+++ spec/wtf/core_spec.rb
@@ -2,5 +2,40 @@ require 'spec_helper'
 
 module Wtf
   describe Core do
+    
+    after(:each) do
+      if Wtf::Core.has_documentation?
+        File.delete(Wtf::Core.current_path + '/.wtf')
+      end
+    end
+    
+    context "#current_path" do
+      describe "without a file" do
+        it "should be a string" do
+          expect(Core.current_path).to be_a String
+        end
+        it "should say if the current folder is documented" do
+          expect(Core.has_documentation?).to be false
+        end
+        it "should clean the directory" do
+          expect(Core.clean).to be nil
+        end
+      end
+
+      describe "with a file" do 
+        
+        before(:each) do 
+          File.open(Wtf::Core.current_path + '/.wtf', 'w') {|f| f.write("Somethin") }
+        end
+        
+        it "should say if the current folder is documented" do
+          expect(Core.has_documentation?).to be true
+        end
+        it "should clean the directory" do
+          expect(Core.clean).to be true
+        end
+
+      end
+    end
   end
 end
